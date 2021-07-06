5052975d6fcf431c72541323e54e8c9144b0490b
Matthew Pollard <k1bbb@outlook.com>
2021-07-06 15:24:57 -0400

    mkp - app update

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
-import {
-  html,
-  render
-} from "https://unpkg.com/htm/preact/standalone.module.js";
+import React from "react";
 import { Header } from "./Components/Header.js";
 import { EmailShare } from "./Components/EmailShare.js";
 import { SubmitAlert } from "./Components/SubmitAlert.js";
 import { BeerListItem } from "./Components/BeerListItem.js";
 
-function App() {
-  return html`
-  <${Header} title="TopTap">
-    Select your favorite beers
-  </${Header}>
-  <form>
-    <!-- Creates checklist of beers from BeerListItem.js -->  
-    <${BeerListItem} />
-    <!-- Creates email text field from EmailShare.js -->
-    <${EmailShare} />
-    <!-- Creates submit button and confirmation dialogue from SubmitAlert.js -->
-    <${SubmitAlert} />
-  </form>
-  `;
-}
+const App = () => {
+  return <Header />;
+  return <EmailShare />;
+  return <SubmitAlert />;
+  return <BeerListItem />;
+};
 
-render(html` <${App} /> `, document.getElementById("app"));
+export default App;
