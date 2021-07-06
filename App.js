fd5e2be238aba4f4bac2e8e0e0ef731ead2c2840
Matthew Pollard <k1bbb@outlook.com>
2021-07-06 15:37:56 -0400

    mkp - final push before going home

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import ReactDOM from "react-dom"
+import { StrictMode } from "react";
 import { Header } from "./Components/Header.js";
 import { EmailShare } from "./Components/EmailShare.js";
 import { SubmitAlert } from "./Components/SubmitAlert.js";
 import { BeerListItem } from "./Components/BeerListItem.js";
 
-const App = () => {
-  return <Header />;
-  return <EmailShare />;
-  return <SubmitAlert />;
-  return <BeerListItem />;
-};
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+ <Header />
+  <EmailShare />
+  <SubmitAlert />
+  <BeerListItem />
+  </StrictMode>,
+  rootElement
+);
 
-export default App;
