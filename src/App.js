78ffd458c3016885088c636264bdc8916c53dbba
Matthew Pollard <k1bbb@outlook.com>
2021-07-07 21:34:46 -0400

    back4app update to app.js and environment.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import Components from "./Components/Components"
+import React from "react";
+import Components from "./Components/Components.js";
+import * as Env from "./environments";
+import Parse from "parse";
+
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
 
 export default function App() {
   return <Components />;
--- src/environments.js
+++ src/environments.js
@@ -1,6 +1,6 @@
 module.exports = {
-    APPLICATION_ID: "x",
-    JAVASCRIPT_KEY: "x",
+    APPLICATION_ID: "VZBVZ56HH76lLu0lxvehca5em735MBOwK8FL8tTO",
+    JAVASCRIPT_KEY: "y10NTI8teCXdjBvQA8A6anFHM6ElyeDibyHtRs89",
     SERVER_URL: "https://parseapi.back4app.com/"
   };
   
