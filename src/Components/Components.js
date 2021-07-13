6e260ccb5677b068e83e37f3c565cc2d86287134
NateKrak <86272084+NateKrak@users.noreply.github.com>
2021-07-13 10:29:53 -0400

    Debug for testing

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -2,7 +2,7 @@ import Header from "./Header/Header.js";
 import Home from "./Home/Home.js";
 import Survey from "./Survey/Survey.js"
 import AuthRegister from "./Auth/AuthRegister.js"
-import AuthLogin from "./Auth/AuthLogin.js"
+// import AuthLogin from "./Auth/AuthLogin.js"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function Components() {
@@ -12,7 +12,7 @@ export default function Components() {
         <Route path="/" component={Header} />
         <Route path="/" exact component={Home} />
         <Route path="/Survey" exact component={Survey} />
-        <Route path="/Login" exact component={AuthLogin} />
+        {/* <Route path="/Login" exact component={AuthLogin} /> */}
         <Route path="/Register" exact component={AuthRegister} />
       </div>
     </Router>
