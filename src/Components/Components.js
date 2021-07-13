19b17e62d1b9a061f20f583842f4cbc60f470810
NateKrak <86272084+NateKrak@users.noreply.github.com>
2021-07-13 10:19:39 -0400

    Auth updates

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,6 +1,8 @@
 import Header from "./Header/Header.js";
 import Home from "./Home/Home.js";
 import Survey from "./Survey/Survey.js"
+import AuthRegister from "./Auth/AuthRegister.js"
+import AuthLogin from "./Auth/AuthLogin.js"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function Components() {
@@ -10,6 +12,8 @@ export default function Components() {
         <Route path="/" component={Header} />
         <Route path="/" exact component={Home} />
         <Route path="/Survey" exact component={Survey} />
+        <Route path="/Login" exact component={AuthLogin} />
+        <Route path="/Register" exact component={AuthRegister} />
       </div>
     </Router>
   );
