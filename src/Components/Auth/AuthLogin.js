e1f45539b8b71ea60aa41dabb08fab56e6715c0a
NateKrak <86272084+NateKrak@users.noreply.github.com>
2021-07-14 20:02:55 -0400

    Auth Login

--- src/Components/Auth/AuthLogin.js
+++ src/Components/Auth/AuthLogin.js
@@ -1,2 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import Parse from "parse";
 
+const UserLogIn = ({ user, onChange, onSubmit }) => {
+    const [username, setUsername] = useState([]);
+    const [password, setPassword] = useState([]);
+
+    const doUserLogIn = async function (){
+        const usernameValue = username;
+        const passwordValue = password;
+        return await Parse.user.login(usernameValue, passwordValue)
+            .then(async (loggedInUser) => {
+                alert(
+                    `User ${loggedInUser.get('username')} has successfully signed in!`
+                );
+                const currentUser = await Parse.User.currentAsync();
+                console.log(loggedInUser === currentUser);
+                return true;
+            });
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    value={username}
+                    placeholder={'Username'}
+                    onChange={onChange}
+                />
+                <input
+                    type="password"
+                    value={password}
+                    placeholder={'Password'}
+                    secureTextEntry
+                    onChange={onChange}
+                />
+                <button type="submit" onSubmit={onSubmit}>Sign in</button>
+            </form>
+        </div>
+    );
+};
+
+export default UserLogIn;
