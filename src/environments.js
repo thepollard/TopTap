2c9053299163244365bedfaacaecae7e290740b4
Matthew Pollard <k1bbb@outlook.com>
2021-07-20 17:55:34 -0400

    Deleting all in mattdev

--- src/environments.js
+++
@@ -1,8 +0,0 @@
-// Importing our keys from Back4App so that we can push and pull changes from the noSQL DB
-
-module.exports = {
-    APPLICATION_ID: "VZBVZ56HH76lLu0lxvehca5em735MBOwK8FL8tTO",
-    JAVASCRIPT_KEY: "y10NTI8teCXdjBvQA8A6anFHM6ElyeDibyHtRs89",
-    SERVER_URL: "https://parseapi.back4app.com/"
-  };
-  
