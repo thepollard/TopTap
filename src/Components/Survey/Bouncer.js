de69e8cdc7a6031fcb277e5ec2f504436368e35e
NateKrak <86272084+NateKrak@users.noreply.github.com>
2021-07-20 17:47:07 -0400

    Fix Bouncer from nateDev merge

--- src/Components/Survey/Bouncer.js
+++ src/Components/Survey/Bouncer.js
@@ -1,8 +1,6 @@
-import EmailShare from "../Email/EmailShare.js";
-import SubmitAlert from "../Submit/SubmitAlert.js";
-import CustomBeers from "../CustomBeers/CustomBeers.js"
-import BeerListItem from "../BeerList/BeerListItem.js";
-import React from "react";
+import ProtectedBeers from "./ProtectedBeers";
+import ProtectedRoute from "../../Common/AppTools/ProtectedRoute";
+import React, { useState } from "react";
 
 // Survey is maybe the wrong term for this module, maybe change this in future.
 export default function Bouncer() {
