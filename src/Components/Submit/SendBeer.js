92bc34f87422dbdaf26a189679643a4357362e62
Matthew Pollard <k1bbb@outlook.com>
2021-07-21 20:56:27 -0400

    I'm done...

--- src/Components/Submit/SendBeer.js
+++ src/Components/Submit/SendBeer.js
@@ -1,8 +1,10 @@
-//this file is the parent to SubmitButton.js and is called by Survey.js
+//this file is the parent to SubmitButton.js and is called by ProtectedBeers.js
 
 import '../Survey/survey.css';
-import BeerMail from "../Email/BeerMail"
-import CustomBeersList from "../CustomBeers/CustomBeersList";
+import BeerMail from "../Email/BeerMail";
+import getAllBeers from "../Services/BottleService";
+
+
 
 const SendBeer = () => {
   return (
@@ -11,20 +13,3 @@ const SendBeer = () => {
   }
 
   export default SendBeer;
-
-  // const SubmitAlert = ({ mailto }) => {
-//   return (
-    
-//       <button
-//           to='#'
-//           onClick={(e) => {
-//               window.location = mailto;
-//               e.preventDefault();
-//           }}
-//       >
-//           Send your BEER!
-//       </button>
-//   );
-// }
-
-// export default SubmitAlert;
